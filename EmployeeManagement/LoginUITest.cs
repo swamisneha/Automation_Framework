8077c3787a803bec3e5195edde92f8dce2c8fa40
snehaps <swamisneha124@gmail.com>
2023-01-03 16:44:29 +0530

    LoginUITest

--- EmployeeManagement/LoginUITest.cs
+++ EmployeeManagement/LoginUITest.cs
@@ -5,16 +5,43 @@ namespace EmployeeManagement
 {
     public class LoginUITest
     {
+        IWebDriver driver;
+     [SetUp]
+        public void BeforeMethod()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.Url = ("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        }
 
+        [TearDown]        
+        public void AfterMethod()
+        {
+            driver.Quit();
+        }
         [Test]
         public void ValidateTitleTest()
+        {
+           
+            string actualTitles = driver.Title;
+            Assert.That(actualTitles, Is.EqualTo("OrangeHRM"));
+        }
+
+        [Test]
+
+        public void ValidatePlaceholderTesr()
         {
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
             driver.Url = ("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-            string actualTitles = driver.Title;
-            Assert.That(actualTitles, Is.EqualTo("OrangeHRM"));
+
+            string actualUsernamePlaceholder = driver.FindElement(By.Name("username")).GetAttribute("placeholder");
+            string actualPasswordPlaceholder = driver.FindElement(By.Name("password")).GetAttribute("placeholder");
+
+            Assert.That(actualUsernamePlaceholder, Is.EqualTo("Username"));
+            Assert.That(actualPasswordPlaceholder, Is.EqualTo("Password"));
         }
     }
 }
